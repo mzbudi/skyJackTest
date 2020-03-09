52203f02f35943bbd1c327b387c33cc29dd83112
MzBudi <ronibudianto361@gmail.com>
2020-03-09 21:03:26 +0700

    geolocation Services

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,34 +1,98 @@
-import React, {Component} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import React, {Component, Fragment} from 'react';
+import {Text, StyleSheet, View, PermissionsAndroid} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-import {Button} from 'react-native-elements';
+import {Button, Icon} from 'react-native-elements';
+import Geolocation from 'react-native-geolocation-service';
 
 class Home extends Component {
-  static navigationOption = {
+  state = {
+    latitude: 0,
+    longitude: 0,
+  };
+
+  async componentDidMount() {
+    const hasLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'SkyJack',
+        message: 'SkyJack access to your location ',
+      },
+    );
+    if (hasLocationPermission) {
+      Geolocation.getCurrentPosition(
+        position => {
+          this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        error => {
+          console.log(error.code, error.message);
+        },
+        {enableHighAccuracy: false, timeout: 15000, maximumAge: 10000},
+      );
+    }
+  }
+
+  static navigationOptions = {
+    header: null,
     headerShown: false,
   };
+
+  handleMove = () => {
+    this.refs.map.animateToRegion(
+      {
+        latitude: this.state.latitude,
+        longitude: this.state.longitude,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+      },
+      2000,
+    );
+  };
+
   render() {
+    const {latitude, longitude} = this.state;
+    console.log(latitude, longitude);
     return (
-      <View style={styles.container}>
-        <MapView
-          ref="map"
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          initialRegion={{
-            latitude: -6.6453504,
-            longitude: 106.7778048,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.015,
-          }}>
-          <Marker
-            coordinate={{
-              latitude: -6.6453504,
-              longitude: 106.7778048,
-            }}
+      <Fragment>
+        <View style={styles.container}>
+          <MapView
+            ref="map"
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            initialRegion={{
+              latitude: -6.1825093,
+              longitude: 106.8276039,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.015,
+            }}>
+            {latitude && longitude ? (
+              <Marker
+                coordinate={{
+                  latitude: latitude,
+                  longitude: longitude,
+                }}
+              />
+            ) : null}
+          </MapView>
+          <Icon
+            raised
+            name="gps-fixed"
+            type="MaterialIcons"
+            color="#f50"
+            containerStyle={{position: 'absolute', right: 50, bottom: 80}}
+            onPress={() => this.handleMove()}
           />
-        </MapView>
-        <Button title="Search" />
-      </View>
+          <Button
+            icon={
+              <Icon type="foundation" name="marker" size={15} color="white" />
+            }
+            title="Location"
+            onPress={this.handleMove}
+          />
+        </View>
+      </Fragment>
     );
   }
 }
